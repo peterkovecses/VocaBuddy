bd07d6bc6a5a5d73488c7dc0bb2f2292d7ff96d0
Peter Kovecses <Peter_Kovecses@epam.com>
2023-08-01 17:23:17 +0200

    Configure ui logging

--- src/VocaBuddy.UI/Program.cs
+++ src/VocaBuddy.UI/Program.cs
@@ -4,6 +4,9 @@ using VocaBuddy.UI;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
+builder.Logging.AddConfiguration(
+    builder.Configuration.GetSection("Logging"));
+
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
