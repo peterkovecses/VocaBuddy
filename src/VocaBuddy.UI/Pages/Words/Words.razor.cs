8d272f8d40f6a5b5a9b6284771c44ca4b29dda68
Peter Kovecses <Peter_Kovecses@epam.com>
2023-08-05 18:03:56 +0200

    Implement pagination

--- src/VocaBuddy.UI/Pages/Words/Words.razor.cs
+++ src/VocaBuddy.UI/Pages/Words/Words.razor.cs
@@ -1,21 +1,53 @@
-﻿namespace VocaBuddy.UI.Pages;
+﻿using System.Text;
 
-public class WordsBase : CustomComponentBase
+namespace VocaBuddy.UI.Pages;
+
+public class WordsBase : ComponentBase
 {
+    private string _filter = string.Empty;
+
     protected List<NativeWordViewModel> Words;
     protected bool Loading = true;
 
     [Inject]
     public IWordService WordService { get; set; }
-    protected string Filter { get; set; } = string.Empty;
-    protected List<NativeWordViewModel> FilteredWords
-        => Words.Where(word => ContainsTerm(word)).ToList();
 
-    protected override async Task OnInitializedAsync()
+    protected string Filter
+    {
+        get => _filter;
+        set
+        {
+            _filter = value;
+            CurrentPage = 1;
+        }
+    }
+
+    protected int CurrentPage { get; set; } = 1;
+    protected int PageSize { get; set; } = 10;
+    protected int TotalItems { get; set; }
+    protected readonly List<int> PageSizes = new() { 5, 10, 25 };
+
+    protected List<NativeWordViewModel> FilteredWords
+        => Words?.Where(word => ContainsTerm(word)).ToList() ?? new List<NativeWordViewModel>();
+
+    protected List<NativeWordViewModel> PagedWords
+        => FilteredWords.Skip((CurrentPage - 1) * PageSize).Take(PageSize).ToList();
+
+    protected async override Task OnInitializedAsync()
     {
         Loading = true;
         Words = await WordService.GetWordsAsync();
         Loading = false;
+        TotalItems = Words.Count;
+    }
+
+    protected void ChangePage(int pageIndex)
+        => CurrentPage = pageIndex;
+
+    protected void ChangePageSize(int size)
+    {
+        PageSize = size;
+        CurrentPage = 1;
     }
 
     private bool ContainsTerm(NativeWordViewModel word)
