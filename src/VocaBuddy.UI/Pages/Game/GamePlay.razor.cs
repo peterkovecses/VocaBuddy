d504769b9c13980c92099181bd0426fda3da8787
Peter Kovecses <Peter_Kovecses@epam.com>
2023-10-22 12:57:56 +0200

    Implement the game

--- src/VocaBuddy.UI/Pages/Game/GamePlay.razor.cs
+++ src/VocaBuddy.UI/Pages/Game/GamePlay.razor.cs
@@ -12,9 +12,93 @@ public class GameplayBase : CustomComponentBase
     public IWordService WordService { get; set; }
 
     protected List<NativeWordDto> Words { get; set; }
+    protected NativeWordDto ActualWord { get; set; }
+    protected List<NativeWordDto> Mistakes { get; set; } = new();
+    protected string UserInput { get; set; } = string.Empty;
+    protected bool IsSubmitted { get; set; }
+    protected bool CorrectAnswer { get; set; }
 
     protected override async Task OnInitializedAsync()
     {
+        ValidateWordCount();
         Words = await WordService.GetWordsAsync(WordCount);
+        SetNextWord();
     }
+
+    protected void EvaluateAnswer()
+    {
+        IsSubmitted = true;
+        SetAnswerCorrectness();
+    }
+
+    protected void NextRound()
+    {
+        IsSubmitted = false;
+        UserInput = string.Empty;
+
+        if (CorrectAnswer)
+        {
+            if (Words.Any())
+            {
+                SetNextWord();
+            }
+            else if (Mistakes.Any())
+            {
+                CountinueWithMistakes();
+                SetNextWord();
+            }
+            else
+            {
+                EndGame();
+            }
+        }
+        else
+        {
+            Mistakes.Add(ActualWord);
+            if (Words.Any())
+            {
+                SetNextWord();
+            }
+            else
+            {
+                CountinueWithMistakes();
+                SetNextWord();
+            }
+        }
+    }
+
+    private void SetAnswerCorrectness()
+    {
+        if (ActualWord.Translations.Any(translation => translation.Text.ToLower() == UserInput.ToLower()))
+        {
+            CorrectAnswer = true;
+        }
+        else
+        {
+            CorrectAnswer = false;
+        }
+    }
+
+    private void ValidateWordCount()
+    {
+        if (WordCount <= 0)
+        {
+            throw new Exception("The number of words must be greater than zero");
+        }
+    }
+
+    private void SetNextWord()
+    {
+        ActualWord = Words.First();
+        Words.Remove(ActualWord);
+    }
+
+    private void CountinueWithMistakes()
+    {
+        Words = Mistakes;
+        Mistakes = new();
+    }
+
+    private void EndGame()
+        => NavManager.NavigateTo("/game-results");
 }
