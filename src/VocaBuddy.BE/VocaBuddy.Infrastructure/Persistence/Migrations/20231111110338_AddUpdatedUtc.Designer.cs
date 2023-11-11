f7f15354754f8384c16b95aa144d7be758902a1d
Peter Kovecses <Peter_Kovecses@epam.com>
2023-11-11 12:07:00 +0100

    Create migration

--- src/VocaBuddy.BE/VocaBuddy.Infrastructure/Persistence/Migrations/20231111110338_AddUpdatedUtc.Designer.cs
+++ src/VocaBuddy.BE/VocaBuddy.Infrastructure/Persistence/Migrations/20231111110338_AddUpdatedUtc.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using VocaBuddy.Infrastructure.Persistence;
 
@@ -11,9 +12,11 @@ using VocaBuddy.Infrastructure.Persistence;
 namespace VocaBuddy.Infrastructure.Persistence.Migrations
 {
     [DbContext(typeof(VocaBuddyContext))]
-    partial class VocaBuddyContextModelSnapshot : ModelSnapshot
+    [Migration("20231111110338_AddUpdatedUtc")]
+    partial class AddUpdatedUtc
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/VocaBuddy.BE/VocaBuddy.Infrastructure/Persistence/Migrations/20231111110338_AddUpdatedUtc.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace VocaBuddy.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUpdatedUtc : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedUtc",
+                table: "NativeWords",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.CreateIndex(
+                name: "IX_NativeWords_UpdatedUtc",
+                table: "NativeWords",
+                column: "UpdatedUtc")
+                .Annotation("SqlServer:Clustered", false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_NativeWords_UpdatedUtc",
+                table: "NativeWords");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedUtc",
+                table: "NativeWords");
+        }
+    }
+}
