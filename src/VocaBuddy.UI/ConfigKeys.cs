8a4d45439885eb7cfa7e044375bad445681b38cf
Peter Kovecses <kovecsespeter@gmail.com>
2024-07-14 12:33:32 +0200

    Add application resilience

--- src/VocaBuddy.UI/ConfigKeys.cs
+++ src/VocaBuddy.UI/ConfigKeys.cs
@@ -4,6 +4,7 @@ public static class ConfigKeys
 {
     public const string IdentityConfiguration = "Apis:Identity";
     public const string VocaBuddyApiConfiguration = "Apis:VocaBuddy";
+    public const string ResilienceConfiguration = "ResilienceConfiguration";
     public const string PasswordConfiguration = "PasswordConfiguration";
     public const string AuthTokenStorageKey = "authToken";
     public const string RefreshTokenStorageKey = "refreshToken";
---
+++ src/VocaBuddy.UI/Models/ResilienceConfiguration.cs
@@ -0,0 +1,22 @@
+﻿namespace VocaBuddy.UI.Models;
+
+public class ResilienceConfiguration
+{
+    public int MaxRetryAttempts { get; set; }
+    public TimeSpan Delay { get; set; }
+    public int BackoffType { get; set; }
+    public TimeSpan AttemptTimeout { get; set; }
+    public TimeSpan TotalRequestTimeout { get; set; }
+    public TimeSpan CircuitBreakerDuration { get; set; }
+
+    public static ResilienceConfiguration CreateDefaultConfiguration()
+        => new()
+        {
+            MaxRetryAttempts = 3,
+            Delay = TimeSpan.FromSeconds(2),
+            BackoffType = 2,
+            AttemptTimeout = TimeSpan.FromSeconds(3),
+            TotalRequestTimeout = TimeSpan.FromSeconds(9),
+            CircuitBreakerDuration = TimeSpan.FromSeconds(20)
+        };
+}
