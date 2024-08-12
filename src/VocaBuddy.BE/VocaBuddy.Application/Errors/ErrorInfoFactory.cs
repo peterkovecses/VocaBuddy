223981bf3cedfead5662f2fb932e56fa61c1d36d
Peter Kovecses <kovecsespeter@gmail.com>
2024-08-12 16:19:25 +0200

    Simplify the creation of error infos

--- src/VocaBuddy.BE/VocaBuddy.Application/Errors/ErrorInfoFactory.cs
+++ src/VocaBuddy.BE/VocaBuddy.Application/Errors/ErrorInfoFactory.cs
@@ -5,22 +5,22 @@ public static class ErrorInfoFactory
     public static ErrorInfo NotFound(object id)
         => new(
             VocaBuddyErrorCodes.NotFound,
-            [new ApplicationError(
+            new ApplicationError(
                     string.Format("Element with id {0} not found.", id),
-                    new KeyValuePair<string, object>(nameof(id), id))]);
+                    new KeyValuePair<string, object>(nameof(id), id)));
 
     public static ErrorInfo UserIdNotMatch()
-        => new(VocaBuddyErrorCodes.UserIdNotMatch, [new ApplicationError("The id of the item does not match the id of the current user")]);
+        => new(VocaBuddyErrorCodes.UserIdNotMatch, new ApplicationError("The id of the item does not match the id of the current user"));
 
     public static ErrorInfo ValidationError(List<ApplicationError> errors)
         => new(VocaBuddyErrorCodes.ValidationError, errors);
 
     public static ErrorInfo Duplicate(string message)
-        => new(VocaBuddyErrorCodes.Duplicate, [new ApplicationError(message)]);
+        => new(VocaBuddyErrorCodes.Duplicate, new ApplicationError(message));
 
     public static ErrorInfo Canceled()
-        => new(VocaBuddyErrorCodes.Canceled, [new ApplicationError("Operation was cancelled.")]);
+        => new(VocaBuddyErrorCodes.Canceled, new ApplicationError("Operation was cancelled."));
 
     public static ErrorInfo ItemCount()
-        => new(VocaBuddyErrorCodes.ItemCount, [new ApplicationError("There are fewer elements in the db than requested.")]);
+        => new(VocaBuddyErrorCodes.ItemCount, new ApplicationError("There are fewer elements in the db than requested."));
 }
