88214cd87cbe666aefb80e1c88d074d7fcb0a0c3
Peter Kovecses <kovecsespeter@gmail.com>
2025-05-06 10:30:37 +0200

    Refactor CustomTokenValidationParameters to use init-only properties

--- src/VocaBuddy.Shared/Models/CustomTokenValidationParameters.cs
+++ src/VocaBuddy.Shared/Models/CustomTokenValidationParameters.cs
@@ -2,20 +2,19 @@
 
 public class CustomTokenValidationParameters
 {
-    public bool ValidateIssuer { get; set; }
-    public bool ValidateAudience { get; set; }
+    public bool ValidateIssuer { get; init; }
+    public bool ValidateAudience { get; init; }
     public bool ValidateLifetime { get; set; }
-    public bool ValidateIssuerSigningKey { get; set; }
-    public string ValidIssuer { get; set; } = default!;
-    public string ValidAudience { get; set; } = default!;
-    public string Secret { get; set; } = default!;
-    public string TokenLifeTime { get; set; } = default!;
-    public string? ClockSkew { get; set; }
+    public bool ValidateIssuerSigningKey { get; init; }
+    public string ValidIssuer { get; init; } = default!;
+    public string ValidAudience { get; init; } = default!;
+    public string Secret { get; init; } = default!;
+    public string TokenLifeTime { get; init; } = default!;
+    public string? ClockSkew { get; init; }
 
 
-    public TokenValidationParameters ToTokenValidationParameters()
-    {
-        return new TokenValidationParameters
+    public TokenValidationParameters ToTokenValidationParameters() =>
+        new()
         {
             ValidateIssuer = this.ValidateIssuer,
             ValidateAudience = this.ValidateAudience,
@@ -26,5 +25,4 @@ public class CustomTokenValidationParameters
             IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(this.Secret)),            
             ClockSkew = this.ClockSkew is not null ? TimeSpan.Parse(this.ClockSkew) : default
         };
-    }
 }
